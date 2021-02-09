f0f4911eaf4e4400954c19504b8fad4014abe591
Viktoriia Nosova <viktoriian@wix.com>
2021-02-09 22:28:28 +0200

    Finished employees page

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -4,11 +4,22 @@ const router = express.Router();
 const pool = require('../dbConfig');
 
 router.get('/', (req, res) => {
+	const sqlQuery = `
+	SELECT 
+		employee_id, 
+		name,
+		COUNT(order_id) AS orders_count
+	FROM employees e
+	LEFT JOIN orders o
+		USING (employee_id)
+	GROUP BY employee_id;
+	`;
+
 	pool.getConnection((err, connection) => {
 		if (err) {
 			return res.status(400).send(err);
 		}
-		connection.query('SELECT * FROM employees', (error, results) => {
+		connection.query(sqlQuery, (error, results) => {
 			if (error) res.status(404).send(error);
 			else res.send(results);
 
