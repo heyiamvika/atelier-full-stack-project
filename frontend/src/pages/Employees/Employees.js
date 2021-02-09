db731a7f413a07e959750d25044adc882dd1d2b1
Viktoriia Nosova <viktoriian@wix.com>
2021-02-09 22:28:50 +0200

    Finished employees page front- and backend

--- frontend/src/pages/Employees/Employees.js
+++ frontend/src/pages/Employees/Employees.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 import { getAllEmployees } from '../../services/employees';
 
+import './Employees.css';
+
 function Employees() {
 	const [employees, setEmployees] = useState([]);
 
@@ -9,15 +11,37 @@ function Employees() {
 		getAllEmployees().then((res) => setEmployees(res));
 	}, []);
 
+	console.log('employees', employees);
+
 	return (
 		<div className='employees'>
 			<div className='page-heading'>
 				<h2>Співробітники</h2>
 			</div>
 			<div className='employees-table'>
-				{employees.map((employee) => (
-					<span key={employee.id}>{employee.name}</span>
-				))}
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>Id співробітника</th>
+							<th scope='col'>ПІБ</th>
+							<th scope='col'>Кількість замовлень</th>
+						</tr>
+					</thead>
+					<tbody>
+						{employees.map((employee) => {
+							console.log(employee);
+							const { employee_id, name, orders_count } = employee;
+
+							return (
+								<tr>
+									<th scope='row'>{employee_id}</th>
+									<td>{name}</td>
+									<td>{orders_count}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
 			</div>
 		</div>
 	);
